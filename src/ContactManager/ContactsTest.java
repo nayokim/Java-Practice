e8118b941ba75a26e004d740dd374c7b2eab8425
Nayoung Kim <nayoung.kimpak@gmail.com>
2020-05-17 20:43:09 -0500

    feat: exit run with message. need to refactor

--- src/ContactManager/ContactsTest.java
+++ src/ContactManager/ContactsTest.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class ContactsTest {
     public static void main(String[] args) {
         boolean running = true;
-        while(running){
+        do {
             System.out.println("1. View contacts.\n" +
                     "2. Add a new contact.\n" +
                     "3. Search a contact by name.\n" +
@@ -13,8 +13,17 @@ public class ContactsTest {
                     "5. Exit.\n" +
                     "Enter an option (1, 2, 3, 4 or 5):");
             Scanner scanner = new Scanner(System.in);
-            int userInput =Integer.parseInt(scanner.nextLine());
-        }
+            int userInput = Integer.parseInt(scanner.nextLine());
+
+            if (userInput == 5) {
+                System.out.println("are you sure you want to exit?");
+                String exitConfirmation = scanner.nextLine().toLowerCase();
+                if (exitConfirmation.equals("y")) {
+                    System.out.println("Have a good day!");
+                    System.exit(0);
+                }
+            }
+        } while (running) ;
 
 
 //            if (userInput == 1){
@@ -34,7 +43,5 @@ public class ContactsTest {
 //            }
 
 
-
-
         }
-}
+    }
